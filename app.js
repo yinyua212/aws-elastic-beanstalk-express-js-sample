4582e595eaa454de8efb201641b8cd18c8bd435a
yinyuan <yinyuan212@gmail.com>
2021-08-01 17:17:47 +0800

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Jojo!'));
+app.get('/', (req, res) => res.send('Gold medal TAI!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
